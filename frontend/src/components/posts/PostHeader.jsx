2a7018e644d6bbd17c8e9845ee4097602e1d0dd1
unknown <lsmxy4@gmail.com>
2026-03-31 20:03:29 +0900

    add update

--- frontend/src/components/posts/PostHeader.jsx
+++ frontend/src/components/posts/PostHeader.jsx
@@ -5,7 +5,7 @@ const PostHeader = ({onCreate}) => {
   return (
     <header className='post-header'>
         <h2 className='post-title'>세 매모를 작성하시오.</h2>
-        <Button text='세 매모 작성' className='primary' icons/>
+        <Button text='세 매모 작성' className='primary' onClick={onCreate} icons/>
     </header>
   )
 }
